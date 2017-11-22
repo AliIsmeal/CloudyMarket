6a1aaebac80a4b12c32b5f1d51b4f1509d121a32
Ali Developer <aliameer1977@gmail.com>
2017-11-22 09:53:29 -0700

    Add project files.

---
+++ Cloudy/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Cloudy.Startup))]
+namespace Cloudy
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
