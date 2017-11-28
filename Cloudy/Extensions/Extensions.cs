7b901abb7081ca6bb1dea14cf3f751b599515d78
Ali Developer <aliameer1977@gmail.com>
2017-11-28 12:57:19 -0700

    Added extension method

---
+++ Cloudy/Extensions/Extensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Cloudy.Extensions
+{
+    public static class ReflectionExtensions
+    {
+        public static string GetPropertyValue<T>(this T Item, string propertyName)
+        {
+            return Item.GetType()
+                .GetProperty(propertyName)
+                .GetValue(Item, null).ToString();
+        }
+    }
+}
